48a7b321bd8bf7bd11cf481ab87c5ba27cba31af
Dede Iskandar <d3215k@gmail.com>
2024-03-06 13:15:24 +0700

    init setup

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/d3215k/go-embed-reactjs/web"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+)
+
+func main() {
+    app := fiber.New()
+
+    app.Use(func(c *fiber.Ctx) error {
+        fmt.Printf("Request: %s %s\n", c.Method(), c.Path())
+        return c.Next()
+	})
+
+    app.Get("/hello", handleHello)
+
+    app.Use("/", filesystem.New(filesystem.Config{
+        Root: web.BuildHTTPFS(),
+        NotFoundFile: "index.html",
+    }))
+
+    log.Fatal(app.Listen(":3000"))
+}
+
+func handleHello(c *fiber.Ctx) error {
+    return c.JSON(fiber.Map{"message": "hello from the fiber server"})
+}
